60b99a82709e73306fca083e9ce785f05d2f50d5
Sherigar Prajna <prajna123sherigar@gmail.com>
2025-06-17 20:35:29 +0530

    Multiple route handlers

--- src/app.js
+++ src/app.js
@@ -3,13 +3,26 @@ const express = require('express')
 const app = express();
 
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello Hello Hello")
-})
-app.use("/test",(req, res)=>{            // response handler
-    res.send("Hello From the server.!11122ee")
-})
+// app.use("/hello",(req,res)=>{
+//     res.send("Hello Hello Hello")
+// })
+// app.use("/test",(req, res)=>{            // response handler
+//     res.send("Hello From the server.!11122ee")
+// })
 
+// app.get("/user",(req,res)=>{
+//     res.send({firstName: 'Sherigar',secondName: 'Prajna'})
+// })
+
+app.get("/user",[(req,res,next)=>{
+console.log("response hander 1");
+// res.send("Respons1")
+next();
+},(req,res,next)=>{
+console.log("response hander 2");
+res.send("Respons2")
+}]
+)
 app.listen(3000,()=>{
     console.log("Server is Running successfully..")
 })
