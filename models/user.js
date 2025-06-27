80e758bc9175a7eea8c82547775b8c60499c511e
Sherigar Prajna <prajna123sherigar@gmail.com>
2025-06-27 11:37:05 +0530

    CRUD operations

---
+++ models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({   
+     firstName : {
+        type: String
+    },
+    lastName : {
+        type: String
+    },
+    emailId : {
+        type: String
+    },
+    password: {
+        type: String
+    }
+})
+
+
+module.exports = mongoose.model("User",userSchema)
--- src/app.js
+++ src/app.js
@@ -1,31 +1,83 @@
-const express = require('express')
-
+const express = require("express");
 const app = express();
-const {adminAuth,userAuth} = require("../middlewers/auth")
+const dbconnect = require("../config/database");
+const User = require("../models/user");
 
+app.use(express.json());
 
-app.use("/admin",adminAuth)
+dbconnect()
+  .then(() => {
+    console.log("DB Connection Established Successfully");
+    app.listen(3000, () => {
+      console.log("Server is Running successfully..");
+    });
+  })
+  .catch((error) => {
+    console.error("Error while Connecting DB", error);
+  });
 
-app.get("/user",userAuth,(req,res)=>{
-    throw new Error("Error....")
-res.send("user success")
-})
-
-app.get("/admin/getUser",(req,res)=>{
-res.send("get success")
+//sign up user
+app.post("/signUp", async (req, res) => {
+  //Creating the instance of the user model
+  const user = await User(req.body);
+  try {
+    user.save();
+    res.send("User inserted");
+  } catch (error) {
+    res.status(400).send("Error while saving the user", +error.message);
+  }
 });
 
-app.get("/admin/deleteUser",(req,res)=>{
-    res.send("delete user api success")
-});
-
-app.use("/",(err,req,res,next)=>{
-    if(err){
-        res.status(500).send("Something went wrong")
+//get users
+app.get("/user", async (req, res) => {
+  try {
+    const users = await User.find({ emailId: req.body.emailID });
+    console.log('users: ', users);
+    if (!users || users.length === 0) {
+      res.status(404).send("No users found with the provided emailID");
+    } else {
+      res.status(200).send(" users found with the same emailID");
     }
-})
+  } catch (error) {
+    res.status(400).send("Error while fetching the users", +error.message);
+  }
+});
 
+//get users
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    console.log('users: ', users);
+    if (!users || users.length === 0) {
+      res.status(404).send("No users found");
+    } else {
+      res.status(200).send(" users found");
+    }
+  } catch (error) {
+    res.status(400).send("Error while fetching the users", +error.message);
+  }
+});
 
-app.listen(3000,()=>{
-    console.log("Server is Running successfully..")
-})
+app.delete("/user", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.body.id);
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    res.status(200).send("User deleted successfully");
+  } catch (error) {
+    res.status(400).send("Error while deleting the user", +error.message);
+  }
+});
+
+app.put("/user", async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.body.id, req.body);
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    res.status(200).send("User updated successfully");
+  } catch (error) {
+    res.status(400).send("Error while updating the user", +error.message);
+  }
+});
